116a0db1cdac0a2c0873564fb87d858398469b5a
slim <slimabida21@gmail.com>
2025-10-29 17:09:44 +0100

    ajout de commentaire dans le productRoute

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -5,6 +5,16 @@ import { getAllProducts } from "../services/productService.js";
 // Un routeur permet d'organiser les routes de manière modulaire
 const router = express.Router();
 
+/**
+ * Route GET pour récupérer tous les produits
+ *
+ * Point de terminaison : /products
+ * Méthode HTTP : GET
+ *
+ * Fonctionnement :
+ * 1. Récupère tous les produits de la base de données
+ * 2. Retourne les produits sous forme de JSON
+ */ 
 router.get("/",  async (req, res) => {
   try {
     const products = await getAllProducts();
